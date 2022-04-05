6bf39742133b47f29b35d71b5a65d44770ead7ce
Brumassio <dibrunspeed@gmail.com>
2022-04-05 18:37:38 -0300

    uc atualizada ! rumo á perfeição !

--- uc.py
+++ uc.py
@@ -4,16 +4,29 @@ class Uc:
         self.pc=[]
         self.count=0
 
-    def setMar(self):
-        self.mar=self.pc[self.count]
 
-    def adicionarInstrucao(self, instrucao:Instrucao):
-        self.pc.append(instrucao)
+    def setMar(self):
+        self.mar=self.pc
+
+    def adicionarEnderecoPc(self, endereco):
+        self.pc.append(endereco)
         
     def  incrementarPc(self):
         self.count+=1
-        self.pc[self.count]
+
+
         
-    def printarPcAtual(self):
-        print(self.pc[self.count].getOpcode())
-        print(self.pc[self.count].getInstrucao())
+    # def printarPcAtual(self):
+    #     print(self.pc[self.count].getOpcode())
+    #     print(self.pc[self.count].getInstrucao())
+
+    # def printarTudo(self):
+    #     while self.uc.count <=len(self.uc.pc)-1:
+    #         self.uc.setMar()
+    #         print(self.uc.pc[self.uc.count])
+    #         self.uc.incrementarPc()    
+
+    def buscaMemoria(self, memory):
+       endereco= int(self.pc[self.count].getEndereco())
+       memory[endereco]
+       self.mbr
