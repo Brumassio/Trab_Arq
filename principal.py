534b36df04e46ea7bb9961aa1867f877704163f3
Brumassio <dibrunspeed@gmail.com>
2022-04-12 19:17:50 -0300

    arquivo principal atualizadinho

--- principal.py
+++ principal.py
@@ -1,37 +1,9 @@
-# salve professor Felippe Fernandes  da Silva ! <3
-# é us guri do python
-
-#simulador do ciclo de instrução do computador.
-# 1) Entrada de dados
-# 2) ULA
-    # 2.1) AC, MQ
-# 3) UC
-    # 3.1) MBR, MAR, IBR, IR, PC
-
-# 4 bits para o opcode e 8 bits de referência dos operandos.
-# ULA deve-se tratar overflow,
-# valores negativos e positivos e realizar os cálculos necessários, além de
-# outros registradores.
-
-# Como implementar memória
-# Como declarar var em Bits
-# opcode e operando
-
-# quer que limita as variaveis? 
-# quer que implemente as operações?  Sim
-# implementar memoria? Sim
-
-from xml.etree.ElementTree import tostring
 from instrucao import Instrucao
 from uc import Uc
 import numpy as np
 from tools import *
+from auxliar import *
 
-
-#0100 Soma
-#0101 Subtração
-#0010 Divisão
-#0001 Multiplicação
 lista = []
 memory=np.array(range(255))
 
@@ -44,9 +16,6 @@ memory=np.array(range(255))
 # memory[2]=Instrucao("0100", "10100000")
 
 # uc.adicionarEnderecoPc("00000010")
-
-
-
 
 
 # uc.printarTudo()
@@ -71,42 +40,27 @@ memory=np.array(range(255))
 #         uc.incrementarPc()
 
 n1="0110"
-n2="1101"
+n2="1010"
 resultado=""
 carryin=0
-# 1011
+
   
 #print(resultado)
 
 
-#Soma
-# for i in range(len(n1)):
-#     aux2 = int(n1[-(i+1)]) ^ int(n2[-(i+1)]) ^ carryin
-#     resultado += str(int(aux2))
-#     if ((int(n1[-(i+1)]) + int(n2[-(i+1)]))==2) or ((int(n1[-(i+1)]) ^ int(n2[-(i+1)]))==1 and carryin==1):
-#           carryin=1
-#     else:
-#         carryin=0
-# resultado += str(carryin)        
-# print(resultado[::-1]) 
+# stringas = "100100"
 
+# soma(n1,n2)
+# igualaCasas(n1,n2)
 
+multiplicacao(n1,n2)
+# stringas = complementoDois(stringas)
+# print(stringas)  
+# print(transformarNegativo(stringas))
 
-
-
-
-stringas = "100100"
-complementoDois(stringas)
-
-
-            
-
-stringas = complementoDois(stringas)
-print(stringas)  
-print(transformarNegativo(stringas))
-
-teste1 = "101"
-teste2 = "111"
-print(multiplicacao(teste1,teste2))
+# teste1 = "101"
+# teste2 = "111"
+# print(multiplicacao(teste1,teste2))
 # print(stringas)
         
+# divisao("1000111","1000")
